b41803ee3b3f81bbb52610596729093e2074de32
Zakaria <1839783@cstj.qc.ca>
2022-09-06 19:29:20 -0400

    Début Exercice3

---
+++ Cours03/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projet1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void openPage1(object sender, RoutedEventArgs e)
+        {
+            page1 p1 = new page1();
+            p1.Show();
+        }
+        private void openPage2(object sender, RoutedEventArgs e)
+        {
+            page2 p2 = new page2();
+            p2.Show();
+        }
+        private void openPage3(object sender, RoutedEventArgs e)
+        {
+            page3 p3 = new page3();
+            p3.Show();
+        }
+    }
+}
